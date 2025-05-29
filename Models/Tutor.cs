041fe63da74363b12795156c91b6ccbffb9d0944
Guilherme Kross <guilhermekross@rede.ulbra.br>
2025-05-28 23:29:06 -0300

    Campo Required na Model Tutor

--- Models/Tutor.cs
+++ Models/Tutor.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Tutor
 {
     public int Id { get; set; }
+    [Required]
     public string? Nome { get; set; }
+    [Required]
     public string? Telefone { get; set; }
     public string? Email { get; set; }
 }
